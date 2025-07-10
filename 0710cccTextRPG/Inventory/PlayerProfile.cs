c9dd7b0a5298a9b6c86f1e74542fc3c78bf1f87c
ccc440440 <ccc440440@gmail.com>
2025-07-10 23:50:16 +0900

    Initial commit

---
+++ 0710cccTextRPG/Inventory/PlayerProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0710cccTextRPG
+{ 
+    public class PlayerProfile
+    {
+        public static int PlayerExp { get; set; } = 0;
+        public static int Level { get; set; } = 1;
+        public static string? PlayerName { get; set; }
+        public static string? Classs { get; set; }
+        public static float AttackPower { get; set; } = 10.0f;
+        public static float ArmorClass { get; set; } = 5.0f;
+        public static float HP { get; set; } = 100.0f;
+        public static int PlayerGold { get; set; } = 1500;
+    }
+}
